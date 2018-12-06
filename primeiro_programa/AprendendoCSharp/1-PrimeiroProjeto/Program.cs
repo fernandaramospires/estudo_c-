d987e6b864801e8cff8cb977dc32fc1f9fcad89e
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-06 21:39:50 -0200

    Primeiro Programa

---
+++ primeiro_programa/AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo, Projeto no Visual Studio");
+            Console.WriteLine(" A execução do acabou. Tecle enter para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
