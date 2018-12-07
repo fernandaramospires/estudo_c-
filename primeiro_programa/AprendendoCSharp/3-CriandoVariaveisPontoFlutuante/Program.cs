199e68e8afba526fe174abb6c3cc059445382109
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-06 23:09:03 -0200

    Criando variáveis ponto flutuante

---
+++ primeiro_programa/AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variáveis ponto flutuante!");
+
+            double salario;
+
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+
+            idade = 15.0 / 2.0;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("A execução terminou. Tecle enter para sair...");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
