bc4d34e91e889a42fca85489eb971e508e56365f
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-11 22:09:44 -0200

    Caracteres e Textos

---
+++ primeiro_programa/AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e Textos");
+
+            // character
+            char primeiraLetra = 'a';
+
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) 65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char) (primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Cursos de tecnologia ";            
+
+            Console.WriteLine(titulo);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
