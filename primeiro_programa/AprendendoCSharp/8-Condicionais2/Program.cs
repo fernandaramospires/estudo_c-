3ae56934ad9ebbd8e8335943583d5f9948af8254
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-11 22:44:44 -0200

    Continuação sobre condicionais

---
+++ primeiro_programa/AprendendoCSharp/8-Condicionais2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 8");
+
+            int idadeJoao = 16;
+            //int quantidadePessoas = 2;
+            //bool acompanhado = quantidadePessoas >= 2;
+
+            bool acompanhado = true;
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("João possui mais de 18 anos de idade. Pode entrar!");
+            }
+            else
+            {
+                 Console.WriteLine("João não possui mais de 18 anos. Não pode entrar");                
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
