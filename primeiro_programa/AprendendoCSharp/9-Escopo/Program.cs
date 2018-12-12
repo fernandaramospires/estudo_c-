b10703ee70393e3686bf3f2922f3b91e48341e45
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-11 23:07:53 -0200

    Escopo

---
+++ primeiro_programa/AprendendoCSharp/9-Escopo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 9");
+
+            int idadeJoao = 16;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+            }
+            else           
+                mensagemAdicional = "João não está acompanhado";
+
+            
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine(" Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine(" Não pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+    }
+}
