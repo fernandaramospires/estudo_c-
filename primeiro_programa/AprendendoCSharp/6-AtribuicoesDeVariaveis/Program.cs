709ac292dd741331b696147a9215260ea56ce73d
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-11 22:19:12 -0200

    Atribuições de Variáveis

---
+++ primeiro_programa/AprendendoCSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 30;
+
+            int idadeFernanda = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade); // imprime 20
+            Console.WriteLine(idadeFernanda); // imprime 30
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
