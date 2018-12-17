1aef0f02add9bb39e19def4ce8d1c360c719092c
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-17 16:32:46 -0200

    Programa que calcula rendimento da poupança

---
+++ primeiro_programa/AprendendoCSharp/10-calculapoupanca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10_calculapoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - calcula poupança");
+            
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            //0.36% = 0.0036
+
+            while (contadorMes  <=  12)
+            {
+
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após {0} meses você terá R$" + valorInvestido, contadorMes);
+                contadorMes++;
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
