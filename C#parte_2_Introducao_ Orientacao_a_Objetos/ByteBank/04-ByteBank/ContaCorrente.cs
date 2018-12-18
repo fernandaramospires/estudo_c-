0e7525e6f296f0588f0253920749549f3fd6fed4
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-18 20:48:55 -0200

    Função sacar na conta corrente

---
+++ C#parte_2_Introducao_ Orientacao_a_Objetos/ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,35 @@
+﻿public class ContaCorrente
+{
+   public string Titular;
+   public int Agencia;
+   public int Numero;
+   public double Saldo = 100;
+
+    public  bool Sacar(double valor)
+    {
+
+        if(this.Saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+
+            this.Saldo -= valor;
+            return true;
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
---
+++ C#parte_2_Introducao_ Orientacao_a_Objetos/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.Titular = "Bruno";
+            Console.WriteLine("Antes do saque, o saldo é de R$ " + contaDoBruno.Saldo);
+
+            bool resultado = contaDoBruno.Sacar(50);
+            Console.WriteLine(resultado);
+
+            Console.WriteLine("Depois do saque, o saldo é de R$" + contaDoBruno.Saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
