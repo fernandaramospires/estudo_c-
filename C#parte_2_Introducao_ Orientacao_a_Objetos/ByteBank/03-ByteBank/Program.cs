6581a19c3e8bd6558d96f06c7c482aca37a390ee
Fernanda Ramos Pires <fpires@id.uff.br>
2018-12-18 20:00:53 -0200

    Exemplo conta corrente, tipos de valor e tipos de referência

---
+++ C#parte_2_Introducao_ Orientacao_a_Objetos/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDaFernanda = new ContaCorrente();
+            contaDaFernanda.Titular = "Fernanda";
+            contaDaFernanda.Agencia = 863;
+            contaDaFernanda.Numero  = 863452;
+
+
+            ContaCorrente contaDaFernandaPires = new ContaCorrente();
+            contaDaFernanda.Titular = "Fernanda";
+            contaDaFernanda.Agencia = 863;
+            contaDaFernanda.Numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: "+ (contaDaFernanda == contaDaFernandaPires)); // retorna false , pois são referências diferentes
+
+            int idade = 26;
+            int idadeMaisUmaVez = 26;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idadeMaisUmaVez));  // retorna true
+
+            contaDaFernanda = contaDaFernandaPires; 
+
+            Console.WriteLine(contaDaFernanda == contaDaFernandaPires); // retorna true, as duas variáveis apontam para o mesmo objeto.
+
+            contaDaFernanda.Saldo = 300;
+
+            Console.WriteLine(contaDaFernanda.Saldo);
+            Console.WriteLine(contaDaFernandaPires.Saldo); // ambos retornam 300, pois ambas variáveis apontam para o mesmo objeto.
+
+            
+            Console.ReadLine();
+        }
+    }
+}
