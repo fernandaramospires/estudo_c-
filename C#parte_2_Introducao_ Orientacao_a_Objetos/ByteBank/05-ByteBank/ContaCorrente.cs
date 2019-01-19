1f9b4f9fdbe100214c1ca9fe40d8c5739c5fb9da
Fernanda Ramos Pires <fpires@id.uff.br>
2019-01-19 20:16:34 -0200

    Referências

---
+++ C#parte_2_Introducao_ Orientacao_a_Objetos/ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,53 @@
+﻿namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int Agencia;
+        public int Numero;
+        public double Saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+
+            if (this.Saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+
+                this.Saldo -= valor;
+                return true;
+            }
+
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.Saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+        
+    }
+}
+
+
+
+
+
+
---
+++ C#parte_2_Introducao_ Orientacao_a_Objetos/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente gabriela = new Cliente();
+
+            gabriela.nome      = "Gabriela";
+            gabriela.profissao = "Desenvolvedora c#";
+            gabriela.cpf       = "245.256.235-89";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+            conta.Saldo   = 500;
+            conta.Agencia = 563;
+            conta.Numero = 5634527;
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.ReadLine();
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
