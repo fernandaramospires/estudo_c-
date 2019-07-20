71473e66b5dcf33030e704a7a35c0a62277b2ffc
Fernanda Pires <fpires@id.uff.br>
2019-07-20 10:17:40 -0300

    C# Parte 3 - Herança, interface e polimorfismo

---
+++ C#parte_3_Heranca_Interface_Polimorfismo/ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor
+    {
+        public string Nome { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ C#parte_3_Heranca_Interface_Polimorfismo/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Peter = new Funcionario(1);
+
+
+
+             
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
